cc5f09531f69650ad83bb678d690ab430bd60ce8
RONY18458 <1460395@senati.pe>
2024-10-03 11:08:35 -0500

    22

--- app/src/main/java/com/example/onlineshop/activity/introActivity.kt
+++ app/src/main/java/com/example/onlineshop/activity/introActivity.kt
@@ -1,17 +1,32 @@
 package com.example.onlineshop.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.onlineshop.MainActivity
 import com.example.onlineshop.R
+import com.example.onlineshop.databinding.ActivityIntroBinding
 
 class introActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_intro)
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding= ActivityIntroBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.starBtn.setOnClickListener{
+            startActivity(Intent(this@introActivity,MainActivity::class.java))
+
+
+
+        }
 
     }
-}
+    }
